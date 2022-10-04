c33002105af45c5e3085461ff0fad778284c7d34
helgituan <helgihe268@nemi.tskoli.is>
2022-10-04 12:55:55 +0000

    Bætta valda með lul

--- Battleground.Repositories/Scripts/population.sql
+++ Battleground.Repositories/Scripts/population.sql
@@ -7,6 +7,7 @@ INSERT INTO "Players" ("Name", "Deleted") VALUES ('Pétur', FALSE); -- 2
 INSERT INTO "Players" ("Name", "Deleted") VALUES ('Helgi', FALSE); -- 3
 INSERT INTO "Players" ("Name", "Deleted") VALUES ('Gummi', FALSE); -- 4
 INSERT INTO "Players" ("Name", "Deleted") VALUES ('Jónas', FALSE); -- 5
+INSERT INTO "Players" ("Name", "Deleted") VALUES ('Valdimar', FALSE); -- 6
 -- Create players here ...
 
 
@@ -15,6 +16,7 @@ INSERT INTO "PlayerInventories" ("PokemonIdentifier", "PlayerId", "AcquiredDate"
 INSERT INTO "PlayerInventories" ("PokemonIdentifier", "PlayerId", "AcquiredDate") VALUES ('charizard', 3, DATE('now'));
 INSERT INTO "PlayerInventories" ("PokemonIdentifier", "PlayerId", "AcquiredDate") VALUES ('bulbasaur', 4, DATE('now'));
 INSERT INTO "PlayerInventories" ("PokemonIdentifier", "PlayerId", "AcquiredDate") VALUES ('squirtle', 5, DATE('now'));
+INSERT INTO "PlayerInventories" ("PokemonIdentifier", "PlayerId", "AcquiredDate") VALUES ('dragonite', 6, DATE('now'));
 -- Create inventories here ...
 
 -- No need to create more battle statuses
@@ -27,6 +29,7 @@ INSERT INTO "Battles" ("WinnerId", "StatusId") VALUES (NULL, 1);
 INSERT INTO "Battles" ("WinnerId", "StatusId") VALUES (3, 3);
 INSERT INTO "Battles" ("WinnerId", "StatusId") VALUES (4, 3);
 INSERT INTO "Battles" ("WinnerId", "StatusId") VALUES (2, 3);
+INSERT INTO "Battles" ("WinnerId", "StatusId") VALUES (6, 3);
 -- Create battles here ...
 
 INSERT INTO "BattlePokemons" ("PokemonIdentifier", "BattleId") VALUES ('snorlax', 1);
@@ -34,6 +37,7 @@ INSERT INTO "BattlePokemons" ("PokemonIdentifier", "BattleId") VALUES ('eevee', 
 INSERT INTO "BattlePokemons" ("PokemonIdentifier", "BattleId") VALUES ('charizard', 3);
 INSERT INTO "BattlePokemons" ("PokemonIdentifier", "BattleId") VALUES ('bulbasaur', 4);
 INSERT INTO "BattlePokemons" ("PokemonIdentifier", "BattleId") VALUES ('squirtle', 5);
+INSERT INTO "BattlePokemons" ("PokemonIdentifier", "BattleId") VALUES ('dragonite', 6);
 -- Create battlepokemons here ...
 
 INSERT INTO "Attacks" ("Success", "CriticalHit", "Damage", "BattleId", "BattlePokemonIdentifier") VALUES (TRUE, TRUE, 400, 1, 'snorlax');
@@ -43,4 +47,5 @@ INSERT INTO "Attacks" ("Success", "CriticalHit", "Damage", "BattleId", "BattlePo
 INSERT INTO "Attacks" ("Success", "CriticalHit", "Damage", "BattleId", "BattlePokemonIdentifier") VALUES (FALSE , FALSE, 0, 4, 'bulbasaur');
 INSERT INTO "Attacks" ("Success", "CriticalHit", "Damage", "BattleId", "BattlePokemonIdentifier") VALUES (TRUE, TRUE, 50, 4, 'bulbasaur');
 INSERT INTO "Attacks" ("Success", "CriticalHit", "Damage", "BattleId", "BattlePokemonIdentifier") VALUES (TRUE, FALSE, 200, 5, 'squirtle');
+INSERT INTO "Attacks" ("Success", "CriticalHit", "Damage", "BattleId", "BattlePokemonIdentifier") VALUES (TRUE, TRUE, 500, 6, 'dragonite');
 -- Create attacks here ...
