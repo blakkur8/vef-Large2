b3e71c22e16cba1be2757ff4191673c52276f2b8
helgituan <helgihe268@nemi.tskoli.is>
2022-10-04 13:53:45 +0000

    Battle Type boi og Attackinputtype ekki klarað eksdje

--- Battleground.Api/Schema/InputTypes/AttackInputType.cs
+++ Battleground.Api/Schema/InputTypes/AttackInputType.cs
@@ -1,8 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using Battleground.Models.InputModels;
+
 namespace Battleground.Api.Schema.InputTypes;
 
-public class AttackInputType
+public class AttackInputType : InputObjectGraphType<AttackInputModel>
 {
-    public string Attacker { get; set; }
-
-    public int BattleId { get; set; }
+    public AttackInputType()
+    {
+        //Field(x => x.Attacker).Description("The name of the attacker");
+        //Field(x => x.battleId).Description("the Id of the battle");
+    }
 }
---
+++ Battleground.Api/Schema/Types/BattleType.cs
@@ -0,0 +1,20 @@
+using GraphQL.Types;
+using Battleground.Models.Dtos;
+
+namespace Battleground.Models.Api.Schema.Types;
+
+public class BattleType : ObjectGraphType<BattleDto>
+{
+    public BattleType()
+    {
+        Field(x => x.Id).Description("The id of the battle");
+        /*
+         * status : BattleStatus
+         * winner : PlayerType
+         * battlePokemons : An array of the Pokemons
+         * playersInMatch : An array of the PlayerType
+         * attacks : an array of the AttackType
+         */
+    }
+    
+}
